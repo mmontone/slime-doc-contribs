b791f84a7a2e88b122f77d4e2e89b3c41a270c58
Mariano Montone <marianomontone@gmail.com>
2021-04-27 08:00:03 -0300

    License

--- slime-info.el
+++ slime-info.el
@@ -1,4 +1,26 @@
-;;; slime-info --- Slime info
+;;; slime-info --- Slime info                -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Mariano Montone
+
+;; Author: Mariano Montone <marianomontone@gmail.com>
+;; URL: https://github.com/mmontone/slime-doc-contribs
+;; Keywords: help, lisp, slime, common-lisp
+;; Version: 0.1
+;; Package-Requires: ((emacs "25"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;; This is a package
 
