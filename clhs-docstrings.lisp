7cb4dbed5d381c14a4090e0ec7e9cb9343e8f01f
Mariano Montone <marianomontone@gmail.com>
2023-05-16 00:37:39 -0300

    clhs-docstrings

--- clhs-docstrings.lisp
+++ clhs-docstrings.lisp
@@ -1,6 +1,7 @@
 (require :clhs)
 (require :alexandria)
 (require :hyperspec-lookup)
+(require :str)
 
 (defpackage :clhs-docstrings
   (:use :cl)
@@ -9,6 +10,15 @@
 (in-package :clhs-docstrings)
 
 (setf hyperspec::*hyperspec-root* (princ-to-string (clhs:hyperspec-root)))
+
+(defmacro condp (predicate &body clauses)
+  "COND using PREDICATE."
+  (let ((pred (gensym)))
+    `(let ((,pred ,predicate))
+       (cond
+	 ,@(loop for clause in clauses
+		 collect `((funcall ,pred ,(car clause))
+			   ,@(cdr clause)))))))
 
 (defun create-file-with-docstrings (file)
   (with-open-file (f file :direction :output
@@ -20,12 +30,28 @@
 	  (let ((hyperspec-text
 		  (with-output-to-string (s)
 		    (uiop:run-program (format nil "html2text -style pretty ~a" hyperspec-file)
-				      :output s))))
-	    (write `(setf (documentation ',symbol t)
-			  ,(concatenate 'string
-				       (or (documentation symbol t) "")
-				       hyperspec-text))
-		   :stream f)
-	    (terpri f)))))))
+				      :output s)))
+		(doc-type (condp (lambda (x) (str:starts-with-p x (pathname-name hyperspec-file)))
+			    ("v_" 'variable)
+			    ("f_" 'function)
+			    ("m_" 'function)
+			    ("t_" 'type)
+			    ("a_" nil) ;; ignore for now
+			    ("d_" nil) ;; ignore for now
+			    ("e_" nil)
+			    (""
+			     ;;(error "Unrecognized: ~a" (pathname-name hyperspec-file))
+			     nil
+			     ))))
+	    (format t ".")
+	    (when doc-type
+	      (write `(setf (documentation ',symbol ',doc-type)
+			    ,(concatenate 'string
+					  (or (documentation symbol doc-type) "")
+					  hyperspec-text))
+		     :stream f)
+	      (terpri f))))))))
 
 ;; (create-file-with-docstrings "/mnt/sdb2/home/marian/src/lisp/slime-star/slime-doc-contribs/clhs-docstrings.gen.lisp")
+
+;; (load "/mnt/sdb2/home/marian/src/lisp/slime-star/slime-doc-contribs/clhs-docstrings.gen.lisp")
